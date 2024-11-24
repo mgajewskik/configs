826e0cca51a36824fb2f7b7ad727a60ed9a2fbe4
mgajewskik <mgajewskik@gmail.com>
2024-11-24 16:10:06 +0100

    Remove nvim configs from repository

--- .config/nvim/lua/cmd.lua
+++
@@ -1,9 +0,0 @@
-local cmd = vim.api.nvim_create_user_command
-
--- vim.cmd([[command! Jfmt :%!jq .]])
--- vim.cmd([[command! MD :! litemdview % &]])
--- vim.cmd([[:command Md ! litemdview %:p & disown]])
-
-cmd("Jfmt", "%!jq --indent 4 .", {})
-cmd("MD", "! litemdview % &", {})
-cmd("Md", "! litemdview %:p & disown", {})
