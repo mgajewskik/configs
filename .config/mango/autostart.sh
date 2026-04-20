95b6c40f85deb77fad3ce19bfea24a2f3b1da08f
mgajewskik <mgajewskik@gmail.com>
2026-04-20 19:53:09 +0200

    run apps as systemd units

--- .config/mango/autostart.sh
+++ .config/mango/autostart.sh
@@ -1,14 +1,12 @@
 #! /bin/bash
 set +e
 
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
-# The next line of command is not necessary. It is only to avoid some situations where it cannot start automatically
-/usr/lib/xdg-desktop-portal-wlr &
+dbus-update-activation-environment --systemd \
+    DISPLAY \
+    WAYLAND_DISPLAY \
+    XDG_RUNTIME_DIR \
+    DBUS_SESSION_BUS_ADDRESS \
+    XDG_SESSION_TYPE \
+    XDG_CURRENT_DESKTOP=wlroots
 
-qs -c noctalia-shell &
-udiskie --tray &
-insync start &
-hypridle &
-
-wl-clip-persist --clipboard regular --reconnect-tries 0 &
-wl-paste --type text --watch cliphist store &
+# systemctl --user restart handy.service
